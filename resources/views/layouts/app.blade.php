ad296a2fae9804e301c16c6260f4f6a437e9b499
José Debuchy <jdebuchy@40q.agency>
2024-01-27 09:54:50 -0300

    Create main layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'My Recipe App')</title>
+
+    @vite(['resources/css/app.scss'])
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="{{ url('/') }}">Recetas de la Abuela</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ url('/') }}">Home</a>
+                    </li>
+                    <!-- Additional navigation items here -->
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="">
+        @yield('content')
+    </div>
+
+    <footer class="footer mt-auto py-3 bg-body-tertiary">
+        <div class="container">
+            <span class="text-body-secondary">Recetas de la Abuela</span>
+        </div>
+    </footer>
+
+    <!-- Bootstrap Bundle with Popper -->
+    @vite(['resources/js/app.js'])
+</body>
+</html>
